d70ccc875c85df07ad4fd0a47bff71567d5dfa78
Parth Jawalekar <141254013+ParthJ1411@users.noreply.github.com>
2023-08-10 13:11:01 +0530

    8-Anagrams.cpp

---
+++ 8-Anagrams.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool Anagrams(const string& str1, const string& str2){
+    if (str1.length()!=str2.length()){
+        return false;
+    }
+    int charFrequency[26] = {0};
+
+    for (char c:str1){
+        charFrequency[c-'a']++;
+    }
+    for (char c:str2){
+        charFrequency[c-'a']--;
+    }
+    for (int freq:charFrequency){
+        if (freq!=0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    string s1,s2;
+
+    cout << "Enter the first string: ";
+    cin >> s1;
+    cout << "Enter the second string: ";
+    cin >> s2;
+
+    if (Anagrams(s1,s2)) {
+        cout<<"True"<<endl;
+    } else {
+        cout<<"False"<<endl;
+    }
+    return 0;
+}
